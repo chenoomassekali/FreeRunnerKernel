b7031768eabfd86a84d459d33f050861077034f8
FelcoX <godxyukarzzyt@gmail.com>
2026-04-27 10:57:41 +0000

    sched/uclamp: Register cpu.uclamp.{min,max}
    Extend CPU's cgroup controller commit
    added the read/write handlers for cpu.uclamp.min and cpu.uclamp.max
    but failed to register them in the cpu_files[] cftype array
    
    Signed-off-by: FelcoX <parbindarsingh@outlook.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -8122,6 +8122,20 @@ static struct cftype cpu_files[] = {
 		.write_u64 = cpu_rt_period_write_uint,
 	},
 #endif
+#ifdef CONFIG_UCLAMP_TASK_GROUP
+	{
+		.name = "uclamp.min",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.seq_show = cpu_uclamp_min_show,
+		.write = cpu_uclamp_min_write,
+	},
+	{
+		.name = "uclamp.max",
+		.flags = CFTYPE_NOT_ON_ROOT,
+		.seq_show = cpu_uclamp_max_show,
+		.write = cpu_uclamp_max_write,
+	},
+#endif
 	{ }	/* Terminate */
 };
 
